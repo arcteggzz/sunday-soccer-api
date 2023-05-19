c2dbd5e406037b9cd76c9d8ab347aa651628cbae
Oghenetega Esedere <arcteggzz@gmail.com>
2023-05-19 13:17:18 +0100

    fix: corrected cors options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
-  "https://sunday-league-soccer-lagos.netlify.app/",
+  "https://sunday-league-soccer-lagos.netlify.app",
 ];
 
 module.exports = allowedOrigins;
