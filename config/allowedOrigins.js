4d1672c21ec0ab9044fca64eb2cf072a357c91c1
Oghenetega Esedere <arcteggzz@gmail.com>
2023-06-03 13:06:42 +0100

    feat: added admin link url to cors options allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
   "https://sunday-league-soccer-lagos.netlify.app",
+  "https://sls-admin-application-fe.vercel.app",
 ];
 
 module.exports = allowedOrigins;
